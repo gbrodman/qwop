40294ab811f833c6c322e6481d363f7aa75d4d0b
S <s>
2012-11-14 19:23:26 -0800

    {"type":"TI", "runs":"0"}

--- Thigh.java
+++ Thigh.java
@@ -10,7 +10,7 @@ public class Thigh {
 	private int yloc;
 	
 	private int width;
-	private int height;
+	private int length;
 	
 	private double angleToBody; // -45 to 90 degrees
 	
@@ -21,15 +21,15 @@ public class Thigh {
 	public Thigh(boolean isLeft) {
 		mass = 8;
 		width = 20;
-		height = 50;;
+		length = 50;;
 		xloc = 200;
 		yloc = 200;
 		if (isLeft) {
-			angleToBody = 10;
+			angleToBody = 10 * pi / 180;
 		} else {
-			angleToBody = 5;
+			angleToBody = 5 * pi / 180;
 		}
-		this.shape = new GLine(xloc, yloc, xloc + Math.cos, 250);
+		this.shape = new GLine(xloc, yloc, xloc + length * Math.sin(angleToBody), yloc + length * Math.cos(angleToBody));
 	}
 	
 	public void drawShape(GraphicsProgram p) {
